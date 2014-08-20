c32edf2b226dcd15d24d38ad7e74819b3e3042e7
vitor-tadashi <vitor.tadashi@gmail.com>
2014-08-20 19:21:56 -0300

    Aprendendo a pegar objetos do repositorio GitHub (Checkout)

---
+++ aprendendo-git-hub/src/com/github/aprendendo/classeDePull.java
@@ -0,0 +1,10 @@
+package com.github.aprendendo;
+
+public class classeDePull {
+
+	public static void main(String[] args) {
+		System.out.println("Aprendendo a pegar objetos do repositorio GitHub (Checkout)");
+
+	}
+
+}
